1ba9224e1bd9b906b3c432f021e2e025c61dac00
Mpreyzner <maria.preyzner@gmail.com>
2017-10-05 16:32:21 +0200

    Separate block into another file

--- main.js
+++ main.js
@@ -2,21 +2,15 @@
 const CryptoJS = require("crypto-js")
     ,express = require("express")
     ,bodyParser = require('body-parser')
-    ,WebSocket = require("ws");
+    ,WebSocket = require("ws")
+    ,Block = require("./block.js")
+    ;
 
 const http_port = process.env.HTTP_PORT || 3001
     ,p2p_port = process.env.P2P_PORT || 6001
     ,initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
-class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
-        this.index = index;
-        this.previousHash = previousHash.toString();
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = hash.toString();
-    }
-}
+
 
 var sockets = [];
 var MessageType = {
